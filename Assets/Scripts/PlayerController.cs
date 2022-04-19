9b62ca5fbd4b450172f3bc0c827fa83616df2c52
Karl <enriquezkharl14@gmail.com>
2022-04-20 02:36:15 +0800

    Jump method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //Player properties
     public float walkSpeed = 10f;
     public float gravity = 20f;
+    public float jumpSpeed = 15f;
+
+    //Input flags
+    private bool _startJump;
+    private bool _releaseJump;
+
+    //Player states
+    public bool isJumping;
 
     private Vector2 _input;
     private Vector2 _moveDirections;
@@ -24,18 +33,48 @@ public class PlayerController : MonoBehaviour
 
         if (_characterController.below)
         {
-
+            if (_startJump)
+            {
+                _startJump = false;
+                _moveDirections.y = jumpSpeed;
+                isJumping = true;
+            }
         }
-        else
+
+        else // In the air
         {
+            if (_releaseJump)
+            {
+                _releaseJump = false;
+                
+                if(_moveDirections.y > 0)
+                {
+                    _moveDirections.y *= .5f;
+                }
+            }
+
             _moveDirections.y -= gravity * Time.deltaTime;
         }
 
         _characterController.Move(_moveDirections * Time.deltaTime);
     }
 
+    //Input Methods
     public void OnMovement(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            _startJump = true;
+        }
+        
+        else if (context.canceled)
+        {
+            _releaseJump = true;
+        }
     }
 }
